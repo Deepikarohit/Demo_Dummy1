c43b6a79415fae03ffb1df834b7a954f5e8a8060
deepi <deepi@MIVANSH>
2024-06-10 15:57:57 +0530

    First Practice Push

---
+++ Demo_DummyProject_Git/src/test/java/contacttest/Tocreatecontact.java
@@ -0,0 +1,11 @@
+package contacttest;
+
+public class Tocreatecontact {
+
+	public static void main(String[] args) {
+		
+		System.out.println("created contact");
+
+	}
+
+}
---
+++ Demo_DummyProject_Git/src/test/java/contacttest/TocreatecontactwithOrganization.java
@@ -0,0 +1,10 @@
+package contacttest;
+
+public class TocreatecontactwithOrganization {
+
+	public static void main(String[] args) {
+		
+		System.out.println("create contact  with organization");
+	}
+
+}
